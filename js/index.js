c43069e846e7d92e5f60e35be945c6b7485985a0
Gustavo Tondin <gus.tondin@gmail.com>
2026-03-25 19:25:27 +0100

    presentation

--- js/index.js
+++ js/index.js
@@ -1,35 +1,41 @@
 // preview window change -------------------------------------------------------------
-const windowSelector = document.querySelectorAll('.topbar__item')
-const previewWindow = document.querySelectorAll('.preview__window')
+const windowSelector = document.querySelectorAll(".topbar__item");
+const previewWindow = document.querySelectorAll(".preview__window");
 
 windowSelector.forEach((window, i) => {
-    window.addEventListener('click', () => {
-        windowSelector.forEach((s) => { s.classList.remove('active') })
-        previewWindow.forEach((p) => { p.classList.remove('active') })
+    window.addEventListener("click", () => {
+        windowSelector.forEach((s) => {
+            s.classList.remove("active");
+        });
+        previewWindow.forEach((p) => {
+            p.classList.remove("active");
+        });
+        
+        window.classList.add("active");
+        previewWindow[i].classList.add("active");
+    });
+});
 
-        previewWindow[i].classList.add('active')
-        windowSelector[i].classList.add('active')
-    })
-})
+
 
 // Color picker -------------------------------------------------------------
 
 const root = document.documentElement;
+const colorInputs = document.querySelectorAll(".color__slot input");
+const colorValue = document.querySelectorAll(".color__value");
 
-const palPrimary = getComputedStyle(root).getPropertyValue('--pal-primary');
-const palSecondary = getComputedStyle(root).getPropertyValue('--pal-secondary');
-const palAccent = getComputedStyle(root).getPropertyValue('--pal-accent');
-const palLight = getComputedStyle(root).getPropertyValue('--pal-light');
-const palDark = getComputedStyle(root).getPropertyValue('--pal-dark');
+colorInputs.forEach((input, i) => {
+    const setColor = () => {
+        root.style.setProperty(input.dataset.color, input.value);
+        colorValue[i].textContent = localStorage.getItem(input.dataset.color);
+        localStorage.setItem(input.dataset.color, input.value);
+    }
+    input.addEventListener("input", setColor);
 
-color__slot
-
-document.querySelectorAll('.color__slot input').forEach((input, i) => {
-    input.addEventListener('input', () => {
-      palPrimary.set... = input.value;
-      textDiv.textContent = input.value;
-      colorDiv.style.color = getContrastColor(input.value);
-    });
+    if (localStorage.getItem(input.dataset.color)) {
+        input.value = localStorage.getItem(input.dataset.color);
+        setColor();
+    }
 });
 
-console.log(palPrimary, palSecondary, palAccent, palPrimary)
+// logo upload -------------------------------------------------------------
